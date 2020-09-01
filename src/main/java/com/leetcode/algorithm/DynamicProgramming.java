5bcb9602a02d3e0a7b36db2f6f0ba363ec398923
沈帅虎 <shuaihu.shen@hand-china.com>
2020-09-01 15:05:50 +0800

    【MOD】  dp 打家劫舍

--- src/main/java/com/leetcode/algorithm/DynamicProgramming.java
+++ src/main/java/com/leetcode/algorithm/DynamicProgramming.java
@@ -3,6 +3,8 @@ package com.leetcode.algorithm;
 import javax.validation.constraints.Max;
 import java.util.Arrays;
 
+import static java.lang.Math.max;
+
 /**
  * 动态规划
  *
@@ -16,13 +18,15 @@ public class DynamicProgramming {
         DynamicProgramming dp = new DynamicProgramming();
         //int[][] grid = {{1,2,1},{1,5,1},{4,2,1}};
         //int[][] grid = {{1,2,4},{3,1,2},{1,5,1}};
-        //int minPathSum = dp.minPathSum(grid);
+        //int minPathSum = dp.climbStairs(4);
         //System.out.println(minPathSum);
 
-        int[] product = {-2, 3, -4};
-        int maxProduct = dp.maxProduct(product);
-        System.out.println(maxProduct);
-
+        //int[] product = {-2, 3, -4};
+        //int maxProduct = dp.maxProduct(product);
+        //System.out.println(maxProduct);
+        int nums[] = {1,2,3,1};
+        int rob = dp.rob(nums);
+        System.out.println(rob);
     }
 
 
@@ -55,15 +59,15 @@ public class DynamicProgramming {
         for (int i = 1; i < nums.length; i++) {
             // 正数
             if (nums[i] > 0) {
-                max[i] = Math.max(nums[i], max[i - 1] * nums[i]);
+                max[i] = max(nums[i], max[i - 1] * nums[i]);
                 min[i] = Math.min(nums[i], min[i - 1] * nums[i]);
             } else {
                 // 负数要 与最小对对比
-                max[i] = Math.max(nums[i], min[i - 1] * nums[i]);
+                max[i] = max(nums[i], min[i - 1] * nums[i]);
                 min[i] = Math.min(nums[i], max[i - 1] * nums[i]);
             }
 
-            s = Math.max(s, max[i]);
+            s = max(s, max[i]);
             System.out.println(i + " --》" + s);
         }
 
@@ -189,5 +193,116 @@ public class DynamicProgramming {
 
     }
 
+    /**
+     * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+     *
+     * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+     *
+     * 注意：给定 n 是一个正整数。
+     *
+     * 示例 1：
+     *
+     * 输入： 2
+     * 输出： 2
+     * 解释： 有两种方法可以爬到楼顶。
+     * 1.  1 阶 + 1 阶
+     * 2.  2 阶
+     * 示例 2：
+     *
+     * 输入： 3
+     * 输出： 3
+     * 解释： 有三种方法可以爬到楼顶。
+     * 1.  1 阶 + 1 阶 + 1 阶
+     * 2.  1 阶 + 2 阶
+     * 3.  2 阶 + 1 阶
+     * 输入： 4
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/climbing-stairs
+     *
+     *
+     */
+    public int climbStairs(int n) {
+
+        if (n <= 2) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i < n + 1; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+            System.out.println(i + " - > " + dp[i] );
+        }
+
+        return dp[n];
+    }
+
+
+    /**
+     *
+     * 98. 打家劫舍
+     *
+     * 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+     * 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+     *
+     * 示例 1：
+     *
+     * 输入：[1,2,3,1]
+     * 输出：4
+     * 解释：偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
+     *      偷窃到的最高金额 = 1 + 3 = 4 。
+     * 示例 2：
+     *
+     * 输入：[2,7,9,3,1]
+     * 输出：12
+     * 解释：偷窃 1 号房屋 (金额 = 2), 偷窃 3 号房屋 (金额 = 9)，接着偷窃 5 号房屋 (金额 = 1)。
+     *      偷窃到的最高金额 = 2 + 9 + 1 = 12 。
+     *  
+     * 提示：
+     *
+     * 0 <= nums.length <= 100
+     * 0 <= nums[i] <= 400
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/house-robber
+     *
+     * dp 方程 dp[i] = max(dp[i-2]+nums[i], dp[i-1])
+     */
+    public int rob(int[] nums) {
+
+        //int pre = 0, cur = 0, tmp;
+        //for(int num : nums) {
+        //    tmp = cur;
+        //    cur = Math.max(pre + num, cur);
+        //    pre = tmp;
+        //}
+        //return cur;
+        //
+        //
+        //if (nums == null) {
+        //    return 0;
+        //}
+        int[] dp = new int[nums.length + 1];
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length > 1) {
+            if (nums[1] >= nums[0]) {
+                dp[0] = nums[0];
+                dp[1] = nums[1];
+            } else {
+                dp[0] = nums[0];
+                dp[1] = nums[0];
+            }
+        }  else {
+            return nums[0];
+        }
+
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i-2]+nums[i], dp[i-1]);
+            System.out.println(i + " --> " + dp[i] );
+        }
+        return dp[nums.length];
+    }
 
 }
--- src/main/java/com/util/CommonUtil.java
+++ src/main/java/com/util/CommonUtil.java
@@ -7,6 +7,7 @@ import java.util.regex.Pattern;
 public class CommonUtil {
     private static Pattern humpPattern = Pattern.compile("[A-Z]");
     private static Pattern linePattern = Pattern.compile("_(\\w)");
+    private static final String ENCODING_UFT_8 = "UTF-8";
 
 
     private CommonUtil() {
@@ -95,8 +96,56 @@ public class CommonUtil {
         return sb.toString().replace("Lines", "LineList");
     }
 
+    public static String stringByU8Byte(String str, int lengthb) {
+        try {
+            if (str == null || str.length() <= 0) {
+                return null;
+            }
+            byte[] buf = str.getBytes(ENCODING_UFT_8);
+            if (lengthb >= buf.length) {
+                return str;
+            }
+            int left = Math.max(lengthb / 3, 1);
+            int right = Math.min(str.length(), lengthb);
+            int length = 0;
+            int middle = 0;
+            int middleLen = 0;
+            while (middleLen != lengthb) {
+                middle = (left + right) / 2;
+                middleLen = str.substring(0, middle).getBytes(ENCODING_UFT_8).length;
+                if (middleLen == lengthb) {
+                    length = middle;
+                    break;
+                } else if (middleLen > lengthb) {
+                    right = middle;
+                } else if (middleLen < lengthb) {
+                    left = middle;
+                }
+                if (left == right || (left + right) / 2 == left) {
+                    if (left == 1 && str.substring(0, 1).getBytes(ENCODING_UFT_8).length > lengthb) {
+                        left = 0;
+                    }
+                    length = left;
+                    break;
+                }
+            }
+            return str.substring(0, length);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
     public static void main(String[] args) {
-        System.out.println(lineToHump("cmf_clm_claim_npcexp_lines"));
+        String str = "【支付失败派生的付款单】-LFH-一键全选前置单据-摘要过长派生测试摘要过长派生测试摘要过长派生测试摘要过长派生测试摘要过长派生测试摘要过长派生测试摘要过长派生测试摘要";
+        System.out.println(str.getBytes().length);
+        System.out.println(stringByU8Byte(str, 240));
+        System.out.println(stringByU8Byte(str, 240).getBytes().length);
+
+        Long ten = new Long(10);
+        System.out.println(("10".equals(new Long(10))));
+        System.out.println(("10".equals(String.valueOf(ten))));
+        System.out.println( ten == 10);
+        System.out.println( ten == null);
     }
 }
